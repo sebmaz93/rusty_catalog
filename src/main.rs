aa4575f3c4abd03422212fe91d1e0940d35cf366
sebmaz <seb.maz93@icloud.com>
2024-08-17 04:11:18 +0400

    impl media description

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String },
+}
+
+impl Media {
+    fn description(&self) -> String {
+        if let Media::Book { title, author } = self {
+            format!("Book: {} by {}", title, author)
+        } else if let Media::Movie { title, director } = self {
+            format!("Movie: {} by {}", title, director)
+        } else if let Media::Audiobook { title } = self {
+            format!("Audiobook: {}", title)
+        } else {
+            String::from("media type not found.")
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let audiobook = Media::Audiobook {
+        title: String::from("Syntax Code"),
+    };
+
+    let nice_movie = Media::Movie {
+        title: "Inception".to_string(),
+        director: "Nolan".into(),
+    };
+
+    let just_a_book = Media::Book {
+        title: String::from("Not a book"),
+        author: "random".to_owned(),
+    };
 }
